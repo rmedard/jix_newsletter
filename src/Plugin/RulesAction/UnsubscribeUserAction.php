e79ab862ada47830b77fcce692b757bce8930c30
Medard Rebero <medard.rebero@gmail.com>
2019-10-06 19:52:43 +0200

    Added custom rules action

---
+++ src/Plugin/RulesAction/UnsubscribeUserAction.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Drupal\jix_newsletter\Plugin\RulesAction;
+
+
+use Drupal;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\rules\Core\RulesActionBase;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class SubscribeUserAction
+ * @package Drupal\jix_newsletter\Plugin\RulesAction
+ *
+ * @RulesAction(
+ *     id = "rules_action_unsubscribe_user",
+ *     label = @Translation("Unsubscribe User Action"),
+ *     category = @Translation("Content"),
+ *     context = {
+ *      "entity" = @ContextDefinition(
+ *          value = "entity",
+ *          label = @Translation("Submission object"),
+ *          description = @Translation("Submitted data")
+ *       )
+ *     }
+ * )
+ */
+class UnsubscribeUserAction extends RulesActionBase
+{
+    protected function doExecute(EntityInterface $entity) {
+        $email = $entity->getElementData('gen_news_email');
+        $config = Drupal::config('jix_newsletter.general.settings');
+        $subscriptionUrl = $config->get('general_newsletter_url');
+        $response = Drupal::httpClient()->delete($subscriptionUrl, array(
+            'json' => array(
+                'email' => $email
+            )));
+        if ($response instanceof ResponseInterface) {
+            Drupal::logger('jix_newsletter')->info(json_encode(json_decode($response, true)));
+        }
+    }
+}
