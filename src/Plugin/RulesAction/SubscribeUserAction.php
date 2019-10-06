3c78aad0a9c4dd6b430da195465610970d80859e
Medard Rebero <medard.rebero@gmail.com>
2019-10-06 20:18:52 +0200

    Added custom rules action

--- src/Plugin/RulesAction/SubscribeUserAction.php
+++ src/Plugin/RulesAction/SubscribeUserAction.php
@@ -39,25 +39,17 @@ class SubscribeUserAction extends RulesActionBase
     {
         $names = $entity->getElementData('gen_news_noms');
         $email = $entity->getElementData('gen_news_email');
-        $active = $entity->getElementData('gen_news_active');
         $config = Drupal::config('jix_newsletter.general.settings');
         $subscriptionUrl = $config->get('general_newsletter_url');
-        $response = null;
-        if (boolval($active)) {
-            $response = Drupal::httpClient()->post($subscriptionUrl, array(
-                'json' => array(
-                    'email' => $email,
-                    'name' => $names,
-                    'newsletterId' => strval($newsletterId)
-                )));
-        } else {
-            $response = Drupal::httpClient()->delete($subscriptionUrl, array(
-                'json' => array(
-                    'email' => $email
-                )));
-        }
+        $response = Drupal::httpClient()->post($subscriptionUrl, array(
+            'json' => array(
+                'email' => $email,
+                'name' => $names,
+                'newsletterId' => strval($newsletterId)
+            )));
         if ($response instanceof ResponseInterface) {
-            Drupal::logger('jix_newsletter')->info(json_encode($response));
+            Drupal::logger('jix_newsletter')->info('Response code: ' . $response->getStatusCode()
+                . ' | Phrase: ' . $response->getBody()->getContents());
         }
     }
 }
